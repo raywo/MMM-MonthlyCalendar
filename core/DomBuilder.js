f36d75b6b646023ecc7e7448d73818f2587814df
Ray Wojciechowski <ray@skillbird.de>
2018-02-21 12:31:23 +0100

    calender is being displayed and populated with (fake) data
    MC-1

--- core/DomBuilder.js
+++ core/DomBuilder.js
@@ -6,32 +6,43 @@ class DomBuilder {
     this.translate = translate;
 
     this.month = 3;
+
+    this.events = this.generateFakeData();
   }
 
 
   getDom() {
     let wrapper = document.createElement("div");
     let table = document.createElement("table");
-    let tHead = document.createElement("thead");
-    let tBody = document.createElement("tbody");
-    let day = 1;
 
+    table.appendChild(this.getTableHeader());
+    table.appendChild(this.getTableBody());
+    table.className = "mcTable";
+
+    wrapper.appendChild(table);
+    wrapper.className = "xsmall";
+
+    return wrapper;
+  }
+
+
+  getTableHeader() {
+    let tHead = document.createElement("thead");
     tHead.appendChild(this.getWeekdaysRow());
+
+    return tHead;
+  }
+
+
+  getTableBody() {
+    let tBody = document.createElement("tbody");
 
     for (let i = 0; i < 5; i++) {
       let row = this.getCalendarRow(i);
       tBody.appendChild(row);
     }
 
-    table.appendChild(tHead);
-    table.appendChild(tBody);
-    table.className = "mcTable";
-    wrapper.appendChild(table);
-    wrapper.className = "xsmall";
-
-    this.getCalendarRow();
-
-    return wrapper;
+    return tBody;
   }
 
 
@@ -39,10 +50,10 @@ class DomBuilder {
     let row = document.createElement("tr");
 
     for (let i = 0; i < 7; i++) {
-      let weekday = moment().weekday(i).format("dd");
+      let weekdayName = moment().weekday(i).format("dd");
       let cell = document.createElement("th");
       cell.className = "mcHeaderWeekday bright";
-      cell.innerHTML = weekday;
+      cell.innerHTML = weekdayName;
 
       row.appendChild(cell);
     }
@@ -54,8 +65,8 @@ class DomBuilder {
   getCalendarRow(weekInMonth) {
     let startOfMonth = moment().month(this.month - 1).startOf("month");
     let endOfMonth = moment().month(this.month - 1).endOf("month");
-    let startDay = startOfMonth.weekday();
-    let endDay = endOfMonth.date();
+    let monthStartsAtDay = startOfMonth.weekday();
+    let lastDayOfMonth = endOfMonth.date();
 
     let row = document.createElement("tr");
 
@@ -64,26 +75,88 @@ class DomBuilder {
       let dayDiv = document.createElement("div");
       dayDiv.className = "mcCalendarDay bright";
 
-      let normalizedDay = i + weekInMonth * 7;
+      let currentDay = i + weekInMonth * 7;
+      let normalizedDay = currentDay - monthStartsAtDay + 1;
 
-      if (normalizedDay >= startDay && normalizedDay - startDay < endDay) {
-        dayDiv.innerHTML = normalizedDay - startDay + 1;
+      if (currentDay >= monthStartsAtDay && normalizedDay <= lastDayOfMonth) {
+        dayDiv.innerHTML = normalizedDay;
         cell.appendChild(dayDiv);
+
+        let date = startOfMonth.clone().add(normalizedDay - 1, "days");
+        cell.appendChild(this.getEventsForDay(date));
       }
 
-      let eventDiv = document.createElement("div");
-      eventDiv.className = "mcEvent";
-      eventDiv.innerHTML = Math.random() < 0.5 ? "ProBaug" : "&nbsp;";
-
-      let locationDiv = document.createElement("div");
-      locationDiv.className = "mcLocation dimmed";
-      locationDiv.innerHTML = "Wiesbaden";
-
-      cell.appendChild(eventDiv);
-      cell.appendChild(locationDiv);
       row.appendChild(cell);
     }
 
     return row;
+  }
+
+
+  getEventsForDay(date) {
+    let result = document.createElement("div");
+
+    let eventDiv = document.createElement("div");
+    eventDiv.className = "mcEvent";
+    eventDiv.innerHTML = this.getEventName(date);
+
+    let locationDiv = document.createElement("div");
+    locationDiv.className = "mcLocation dimmed";
+    locationDiv.innerHTML = this.getEventLocation(date);
+
+    result.appendChild(eventDiv);
+    result.appendChild(locationDiv);
+
+    return result;
+  }
+
+
+  getEventName(date) {
+    let dateKey = date.format("DD.MM.YYYY");
+
+    if (this.events[dateKey]) {
+      return this.events[dateKey].name;
+    } else {
+      return "&nbsp;";
+    }
+  }
+
+
+  getEventLocation(date) {
+    let dateKey = date.format("DD.MM.YYYY");
+
+    if (this.events[dateKey]) {
+      return this.events[dateKey].location;
+    } else {
+      return "&nbsp;";
+    }
+  }
+
+
+  // TODO: remove
+  generateFakeData() {
+    return {
+      "05.03.2018": {
+        name: "??ProBAUG",
+        location: "Wiesbaden"
+      },
+      "06.03.2018": {
+        name: "??ProBAUG",
+        location: "Wiesbaden"
+      },
+      "07.03.2018": {
+        name: "??ProBAUG",
+        location: "Wiesbaden"
+      },
+
+      "19.03.2018": {
+        name: "??agile Softwareentwicklung",
+        location: "Wuppertal"
+      },
+      "20.03.2018": {
+        name: "??agile Softwareentwicklung",
+        location: "Wuppertal"
+      }
+    };
   }
 }
